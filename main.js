b1c7aff1f7e0e811346c6651e6dec08ae1fc683f
Victor Sriqui <victorsriqui@gmail.com>
2023-04-08 15:27:05 -0600

    Add login to main transition

--- main.js
+++ main.js
@@ -3,13 +3,31 @@ var buddha = document.querySelector(".buddha");
 var affirmationButton = document.getElementById("affirmation");
 var mantraButton = document.getElementById("mantra");
 var receiveButton = document.querySelector(".receive");
+var enterButton = document.querySelector(".enter");
 
 var message = document.querySelector(".message");
+
+var whichHeader = document.querySelector(".which");
+var buttonSection = document.querySelector(".button");
+var loginSection = document.querySelector(".login");
+var messagesSection = document.querySelector(".messages");
 
 receiveButton.onclick = displayMessage;
 mantraButton.onclick = unclickAffirmation
 affirmationButton.onclick = unclickMantra
-
+onload = function () {
+    whichHeader.style.display = "none";
+    buttonSection.style.display = "none";
+    messagesSection.style.display = "none";
+    // message.style.setProperty("display","block");
+}
+enterButton.onclick = function () {
+    whichHeader.style.display = "block";
+    buttonSection.style.display = "flex";
+    messagesSection.style.display = "flex";
+    loginSection.style.display = "none";
+    // message.style.setProperty("display","block");
+}
 
 function hide(element) {
     element.classList.add("hidden")
@@ -45,6 +63,6 @@ function displayMessage() {
     } else if(mantraButton.checked) {
         message.innerText = chooseMantra();            
     } else {
-        message.innerText = "You can't receive, if you dont choose.";       
+        message.innerText = "You can't receive, if you don't choose.";       
     }
 }
